94365eefd87a9a82df9e01dae88cd15926fdae7a
Ben Bowes <bowesdesign@gmail.com>
2017-11-06 13:39:36 +0800

    Implement basic TextField component
    Signed-off-by: Alan Yeo <ayeo@pivotal.io>

---
+++ src/TextField.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './TextField.css'
+
+class TextField extends Component {
+    render() {
+        return (
+            <fieldset className="textfield">
+                <label className="textfield__label">{ this.props.label }</label>
+                <input className="textfield__input" type="text" />
+            </fieldset>
+        )
+    }
+}
+
+export default TextField;
